8edc496fd81adec02b29f01281163042d9030384
TotoB12 <91705868+TotoB12@users.noreply.github.com>
2023-02-09 17:25:59 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+import epson_projector as epson
+from epson_projector.const import (PWR_OFF)
+
+import asyncio
+import aiohttp
+
+
+async def main():
+    async with aiohttp.ClientSession() as session:
+        await run(session)
+
+
+async def run(websession):    
+    projector = epson.Projector(
+        host='HOSTNAME',
+        websession=websession,
+        encryption=True)
+    data = await projector.send_command(PWR_OFF)
+    print(data)
+
+asyncio.get_event_loop().run_until_complete(main()) 
