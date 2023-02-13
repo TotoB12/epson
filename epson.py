914b550e6f4ed4ab4f4d04abe772130a0df40bca
TotoB12 <91705868+TotoB12@users.noreply.github.com>
2023-02-13 09:17:36 -0500

    Update epson.py

--- epson.py
+++ epson.py
@@ -1,17 +1,20 @@
+import epson_projector as epson
+from epson_projector.const import (PWR_OFF)
+
 import asyncio
 import aiohttp
-import epson_projector as epson
-from epson_projector.const import PWR_OFF
+
 
 async def main():
     async with aiohttp.ClientSession() as session:
-        projects = await epson.discover(session)
-        tasks = [turn_off(p, session) for p in projects]
-        await asyncio.gather(*tasks)
+        await run(session)
 
-async def turn_off(projector, session):
-    p = epson.Projector(host=projector, websession=session)
-    data = await p.send_command(PWR_OFF)
-    print(f"Turned off {projector}: {data}")
 
-asyncio.get_event_loop().run_until_complete(main())
+async def run(websession):    
+    projector = epson.Projector(
+        host=input('Enter IP Address: '),
+        websession=websession)
+    data = await projector.send_command(PWR_OFF)
+    print(data)
+
+asyncio.get_event_loop().run_until_complete(main()) 
