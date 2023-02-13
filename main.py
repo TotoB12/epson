bc4c6033895d839aa1a174d9d363dccfb53ebff5
TotoB12 <91705868+TotoB12@users.noreply.github.com>
2023-02-13 09:14:18 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,15 +4,32 @@ from epson_projector.const import (PWR_OFF)
 import asyncio
 import aiohttp
 
+# L-102  172.16.16.162
+# L-104 172.16.16.119
+# L-105  172.16.16.173
+# L-201  172.16.16.153
+# L-205  172.16.16.168
+# L-206  172.16.16.171
+# C-10  172.16.16.154
+# C-12  172.16.16.167
+# C-22 172.16.16.160
+# C-24  172.16.16.149
+# C-27  172.16.16.169
+# C-ART  172.16.16.155
+projectors = [172.16.16.162, 172.16.16.119, 172.16.16.173, 172.16.16.153, 172.16.16.168, 172.16.16.171, 172.16.16.154, 172.16.16.167, 172.16.16.160, 172.16.16.149, 172.16.16.169, 172.16.16.155]
 
 async def main():
     async with aiohttp.ClientSession() as session:
-        await run(session)
+        for projector in projectors:
+            try:
+                await run(session, projector)
+                print(f'Turned Off {projector}')
+            except Exception as e:
+                print(f'Could not turn off {projector}: {e}')
 
-
-async def run(websession):    
+async def run(websession, projector):    
     projector = epson.Projector(
-        host=input('Enter IP Address: '),
+        host=projector,
         websession=websession)
     data = await projector.send_command(PWR_OFF)
     print(data)
