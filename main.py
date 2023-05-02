0a7970d5292db92265cfdb32dfe96ea00eb5605c
TotoB12 <91705868+TotoB12@users.noreply.github.com>
2023-05-02 13:11:39 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,39 +3,42 @@ from epson_projector.const import (PWR_OFF)
 
 import asyncio
 import aiohttp
+import ipaddress
 
-# L-102  172.16.16.162
-# L-104 172.16.16.119
-# L-105  172.16.16.173
-# L-201  172.16.16.153
-# L-205  172.16.16.168
-# L-107 172.16.68.184
-# L-203 172.16.68.91
-# L-205 172.16.69.23
-# L-206  172.16.16.171
-# C-10  172.16.16.154
-# C-12  172.16.16.167
-# C-22 172.16.16.160
-# C-24  172.16.16.149
-# C-25 172.16.16.164
-# C-27  172.16.16.169
-# C-ART  172.16.16.155
-projectors = [172.16.16.162, 172.16.16.119, 172.16.16.173, 172.16.16.153, 172.16.16.168, 172.16.16.171, 172.16.16.154, 172.16.16.167, 172.16.16.160, 172.16.16.149, 172.16.16.169, 172.16.16.155, 172.16.68.184, 172.16.68.91, 172.16.69.23]
 
 async def main():
     async with aiohttp.ClientSession() as session:
-        for projector in projectors:
-            try:
-                await run(session, projector)
-                print(f'Turned Off {projector}')
-            except Exception as e:
-                print(f'Could not turn off {projector}: {e}')
+        await run(session)
 
-async def run(websession, projector):    
-    projector = epson.Projector(
-        host=projector,
-        websession=websession)
-    data = await projector.send_command(PWR_OFF)
-    print(data)
 
-asyncio.get_event_loop().run_until_complete(main()) 
+async def run(websession):
+    # Use your local network subnet and starting IP for projectors
+    network = ipaddress.ip_network('172.16.16.0/24')
+    starting_ip = ipaddress.IPv4Address('172.16.16.100')
+    tasks = []
+    concurrency_limit = 25  # Set your preferred concurrency limit
+
+    # Create a semaphore with a specified concurrency limit
+    semaphore = asyncio.Semaphore(concurrency_limit)
+
+    for ip in network:
+        if ip >= starting_ip:
+            tasks.append(turn_off_projector(websession, str(ip), semaphore))
+
+    await asyncio.gather(*tasks)
+
+
+async def turn_off_projector(websession, ip, semaphore):
+    async with semaphore:
+        try:
+            projector = epson.Projector(
+                host=ip,
+                websession=websession
+            )
+            data = await projector.send_command(PWR_OFF)
+            print(f"Projector at {ip} turned off: {data}")
+        except Exception as e:
+            print(f"Error turning off projector at {ip}: {e}")
+
+
+asyncio.get_event_loop().run_until_complete(main())
